6a9f830df13b79e86f3936e9673c933e3111bc8a
asifadnan7 <asif.adnan.cse@gmail.com>
2021-03-07 11:15:59 +0600

    setup fetchProperties thunk and store data in properties store

--- src/components/business/propertiesSlice.ts
+++ src/components/business/propertiesSlice.ts
@@ -1,6 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppThunk } from '../../store/store'; //RootState
+
+import axios from '../../utils/axios';
 import { ApiStatus } from '../../enums/api-status.enum';
 import { IProperty } from '../../models/property';
+
+const propertiesUrl = '/properties';
 
 export interface IPropertiesState {
   status: ApiStatus;
@@ -17,15 +22,44 @@ const initialState: IPropertiesState = {
 };
 
 export const counterSlice = createSlice({
-  name: 'property',
+  name: 'properties',
   initialState,
   reducers: {
+    setStatus(state, action: PayloadAction<ApiStatus>) {
+      state.status = action.payload;
+    },
+    setProperties(state, action: PayloadAction<IProperty[]>) {
+      state.data = action.payload;
+    },
+    setError(state, action: PayloadAction<any>) {
+      state.error = action.payload;
+    },
+
     setSelectedPropertyId: (state, action: PayloadAction<string>) => {
       state.selectedPropertyId = action.payload;
     },
   },
 });
 
-export const { setSelectedPropertyId } = counterSlice.actions;
+export const fetchProperties = (): AppThunk => async (dispatch, getState) => {
+  dispatch(setStatus(ApiStatus.LOADING));
+  var response = null;
+  try {
+    response = await axios.get(propertiesUrl);
+  } catch (error) {
+    response = error.message;
+    // TODO: serialize error obj in response interceptor
+  }
+
+  if (response && response.status === 200) {
+    dispatch(setStatus(ApiStatus.SUCCESS));
+    dispatch(setProperties(response.data));
+  } else {
+    dispatch(setStatus(ApiStatus.FAILED));
+    dispatch(setError(response));
+  }
+};
+
+export const { setStatus, setProperties, setError, setSelectedPropertyId } = counterSlice.actions;
 
 export default counterSlice.reducer;
